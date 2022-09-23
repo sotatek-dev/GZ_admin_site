378b5ecd45fa4eebe392b56f120f006f43ef30f6
Hiep Nguyen Van <hiep.nguyen2@sotatek.com>
2022-09-23 14:47:33 +0700

    fix: redirect to admin list on admin detail update succeed

--- src/modules/common/constants/messages.ts
+++ src/modules/common/constants/messages.ts
@@ -7,6 +7,8 @@ export const MESSAGES = {
 	MC5: 'Refresh successfully',
 	MC6: 'Invalid First name',
 	MC7: 'Invalid Last name',
+	MC8: 'Update successfully',
+	MC9: 'Update failed',
 	MSC115: 'This field is required',
 	MSC121: 'Invalid Wallet Address',
 	MSC110: 'Please enter a correct email, example "abc@mail.com"',
--- src/modules/profile/Profile.mutation.ts
+++ src/modules/profile/Profile.mutation.ts
@@ -3,6 +3,9 @@ import { message } from '@common/components';
 import { axiosClient } from '@common/services/apiClient';
 import { Admin } from '@admins/common/types';
 import { API_PROFILE } from '@common/services/queries/useGetProfile';
+import { MESSAGES } from '@common/constants/messages';
+import { useNavigate } from 'react-router';
+import { PATHS } from '@common/constants/paths';
 
 const API = '/profile/admin';
 
@@ -19,14 +22,16 @@ async function updateFn(rqBody: Admin) {
 export const useUpdateProfile = () => {
 	const queryClient = useQueryClient();
 	const updateMutation = useMutation(updateFn);
+	const navigate = useNavigate();
 
 	const updateProfile = async (newAdmin: Admin) => {
 		try {
 			await updateMutation.mutateAsync(newAdmin);
 			await queryClient.invalidateQueries([API_PROFILE]);
-			message.success('Update succeed');
+			message.success(MESSAGES.MC8);
+			navigate(PATHS.admins.list());
 		} catch (error) {
-			message.error('Update failed');
+			message.error(MESSAGES.MC9);
 		}
 	};
 
