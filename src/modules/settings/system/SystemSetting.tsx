da4c5da48e23449caf142457e72cd71e12005cf9
Tỷ Nguyễn <tynguyen@Tys-MacBook-Pro.local>
2022-10-10 18:38:08 +0700

    fix: validate input

--- src/modules/settings/system/SystemSetting.tsx
+++ src/modules/settings/system/SystemSetting.tsx
@@ -1,34 +1,40 @@
 import './SystemSetting.style.scss';
-import {
-	Button,
-	Card,
-	Col,
-	Form,
-	Input,
-	InputNumber,
-	Loading,
-} from '@common/components';
+import { Button, Card, Col, Form, Input, Loading } from '@common/components';
 import { addressValidator, requiredValidate } from '@common/helpers/validate';
 import { useRedirectBack } from '@common/hooks';
 import { MESSAGES } from '@common/constants/messages';
 import useSetting from './services/useSetting';
+const TitleComponent = ({ title }: { title: string }) => {
+	return (
+		<p className='system-setting-form__title'>
+			{title}
+			<span className='system-setting-form__title__star'>*</span>
+		</p>
+	);
+};
+const ErrorComponent = ({ value }: { value: string }) => {
+	return value === '' || value === '0' ? (
+		<p className='system-setting-form__titleError'>{MESSAGES.MSC115}</p>
+	) : (
+		<div></div>
+	);
+};
 export default function SystemSetting() {
 	const goBack = useRedirectBack();
-	const defaultPriceType = 'BUSD';
 	const {
-		disableUpdateBtn,
-		handleFieldChange,
 		isLoadingSystemStatus,
 		handleSubmit,
-		price,
+		fieldCommon,
+		disableUpdateBtn,
+		handleFieldChange,
+		handleRegexField,
 		form,
 	} = useSetting();
-	const formatPrice = (price: number | string) => {
-		return `${price}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-	};
+	const defaultPriceType = 'BUSD';
 	if (isLoadingSystemStatus) {
 		return <Loading />;
 	}
+	console.log(fieldCommon);
 	return (
 		<>
 			<Button onClick={goBack}>Back</Button>
@@ -40,12 +46,11 @@ export default function SystemSetting() {
 				>
 					<Form
 						form={form}
-						onFieldsChange={handleFieldChange}
 						layout='vertical'
+						onFieldsChange={handleFieldChange}
 						name='basic'
 						onFinish={handleSubmit}
 						autoComplete='off'
-						// initialValues={setting}
 					>
 						<Form.Item
 							label='Treasury Address'
@@ -59,64 +64,68 @@ export default function SystemSetting() {
 						>
 							<Input />
 						</Form.Item>
-						<Form.Item
-							wrapperCol={{ span: 24 }}
-							label='Key Price'
-							name='key_price'
-							rules={[{ required: true, message: MESSAGES.MSC115 }]}
-						>
-							<InputNumber
-								min={price.min}
-								max={price.max}
-								defaultValue={price.min}
-								formatter={(value) => formatPrice(value || 0)}
-								addonAfter={defaultPriceType}
-							/>
-						</Form.Item>
-						<Form.Item
-							label='Rescue Price'
-							name='rescure_price'
-							rules={[{ required: true, message: MESSAGES.MSC115 }]}
-						>
-							<InputNumber
-								min={price.min}
-								max={price.max}
-								defaultValue={price.min}
-								formatter={(value) => formatPrice(value || 0)}
-								addonAfter={defaultPriceType}
-							/>
-						</Form.Item>
-						<Form.Item
-							label='Launch Price'
-							name='launch_price'
-							rules={[{ required: true, message: MESSAGES.MSC115 }]}
-						>
-							<InputNumber
-								min={price.min}
-								max={price.max}
-								defaultValue={price.min}
-								formatter={(value) => formatPrice(value || 0)}
-								addonAfter={defaultPriceType}
-							/>
-						</Form.Item>
-						<Form.Item
-							label='Users may mint key for the first (x) days of the month'
-							name='mint_days'
-							rules={[{ required: true, message: MESSAGES.MSC115 }]}
-						>
-							<InputNumber
-								min={price.mintKeyDefault}
-								defaultValue={price.mintKey}
-							/>
-						</Form.Item>
-						<Form.Item
-							label='User must have minimum (x) token to mint key'
-							name='key_mint_min_token'
-							rules={[{ required: true, message: MESSAGES.MSC115 }]}
-						>
-							<InputNumber defaultValue={0} min={price.min} max={price.max} />
-						</Form.Item>
-						<Form.Item style={{ textAlign: 'center' }}>
+						<TitleComponent title='Key Price' />
+						<Input
+							suffix={defaultPriceType}
+							onChange={(e) => handleRegexField(e, 'key_price')}
+							value={fieldCommon.key_price}
+							status={
+								fieldCommon?.key_price === '' || fieldCommon?.key_price === '0'
+									? 'error'
+									: ''
+							}
+						/>
+						<ErrorComponent value={fieldCommon?.key_price} />
+						<TitleComponent title='Rescue Price' />
+						<Input
+							suffix={defaultPriceType}
+							onChange={(e) => handleRegexField(e, 'rescure_price')}
+							value={fieldCommon.rescure_price}
+							status={
+								fieldCommon?.rescure_price === '' ||
+								fieldCommon?.rescure_price === '0'
+									? 'error'
+									: ''
+							}
+						/>
+						<ErrorComponent value={fieldCommon?.rescure_price} />
+						<TitleComponent title='Launch Price' />
+						<Input
+							suffix={defaultPriceType}
+							onChange={(e) => handleRegexField(e, 'launch_price')}
+							value={fieldCommon.launch_price}
+							status={
+								fieldCommon?.launch_price === '0' ||
+								fieldCommon?.launch_price === ''
+									? 'error'
+									: ''
+							}
+						/>
+						<ErrorComponent value={fieldCommon?.launch_price} />
+						<TitleComponent title='Users may mint key for the first (x) days of the month' />
+						<Input
+							onChange={(e) => handleRegexField(e, 'mint_days')}
+							value={fieldCommon.mint_days}
+							status={
+								fieldCommon?.mint_days === '0' || fieldCommon?.mint_days === ''
+									? 'error'
+									: ''
+							}
+						/>
+						<ErrorComponent value={fieldCommon?.mint_days} />
+						<TitleComponent title='User must have minimum (x) token to mint key' />
+						<Input
+							onChange={(e) => handleRegexField(e, 'key_mint_min_token')}
+							value={fieldCommon.key_mint_min_token}
+							status={
+								fieldCommon?.key_mint_min_token === '' ||
+								fieldCommon?.key_mint_min_token === '0'
+									? 'error'
+									: ''
+							}
+						/>
+						<ErrorComponent value={fieldCommon?.key_mint_min_token} />
+						<Form.Item className='system-setting-form__btn'>
 							<Button disabled={disableUpdateBtn} htmlType='submit'>
 								Update
 							</Button>
--- src/modules/settings/system/services/useSetting.ts
+++ src/modules/settings/system/services/useSetting.ts
@@ -1,15 +1,22 @@
 import { Form } from '@common/components';
-import { useCallback, useEffect, useState } from 'react';
-import { FieldData } from 'rc-field-form/es/interface';
+import { useEffect, useState, ChangeEvent, useCallback } from 'react';
 import { MESSAGES } from '@common/constants/messages';
+import { FieldData } from 'rc-field-form/es/interface';
 import { message } from '@common/components';
 import { useUpdateKeyNFTSC } from '@admins/setting/mutations/useUpdateKeyNFTSC';
 import { useUpdateDNFTSC } from '@admins/setting/mutations/useUpdateDNFTSC';
 import { UseQueryResult } from 'react-query';
 import BigNumber from 'bignumber.js';
 import { ContractReceipt } from 'ethers';
-import { InitialProps, SubmitProps } from '../type';
+import { InitialProps } from '../type';
 import { useGetSystemSetting, useUpdateSystem } from './useGetSystemSetting';
+type FieldCommon<T> = {
+	mint_days: T;
+	key_mint_min_token: T;
+	launch_price: T;
+	rescure_price: T;
+	key_price: T;
+};
 const useSetting = () => {
 	const {
 		updateRescurPriceDNFTSC,
@@ -22,14 +29,20 @@ const useSetting = () => {
 		updateTreasuryAddressKeyNFTSC,
 		updatePriceKeyNFTSC,
 	} = useUpdateKeyNFTSC();
-	const [disableUpdateBtn, setDisableUpdateBtn] = useState<boolean>(true);
 	const { data: initialData } = useGetSystemSetting() as UseQueryResult<
 		InitialProps,
 		unknown
 	>;
 	const [form] = Form.useForm();
 	const reloadTime = 500;
-
+	const [disableUpdateBtn, setDisableUpdateBtn] = useState<boolean>(true);
+	const [fieldCommon, setFieldCommon] = useState<FieldCommon<string>>({
+		mint_days: '0', // need number but right now is string
+		key_mint_min_token: '0',
+		launch_price: '0',
+		rescure_price: '0',
+		key_price: '0',
+	});
 	const price: {
 		min: number;
 		mintKey: number;
@@ -46,112 +59,199 @@ const useSetting = () => {
 	const { updateMintDaySystemAdmin, isLoadingSystem } = useUpdateSystem();
 	const [isLoadingSystemStatus, setIsLoadingSystemStatus] =
 		useState<boolean>(isLoadingSystem);
+	const handleRegexField = (
+		e: ChangeEvent<HTMLInputElement>,
+		type:
+			| 'mint_days'
+			| 'key_mint_min_token'
+			| 'launch_price'
+			| 'rescure_price'
+			| 'key_price'
+	) => {
+		// change any field then enable btn update
+		setDisableUpdateBtn(false);
+		const { value } = e.target;
+		const regexCharacterExceptDot = /^[0-9\s.]+$/;
+		if (value) {
+			const spliceDot = value.split('.');
+			const indexBeforeDot = 0;
+			const indexAfterDot = 1;
+			if (regexCharacterExceptDot.test(value)) {
+				switch (spliceDot.length) {
+					case 1: {
+						if (value.length <= 10) {
+							if (parseInt(value) <= price.max) {
+								setFieldCommon({ ...fieldCommon, [type]: value });
+							}
+						}
+						break;
+					}
+					case 2: {
+						if (
+							spliceDot[indexAfterDot].length <= 2 &&
+							spliceDot[indexBeforeDot].length <= 10 &&
+							value !== '.'
+						) {
+							if (parseInt(spliceDot[indexBeforeDot]) <= price.max) {
+								setFieldCommon({ ...fieldCommon, [type]: value });
+							}
+						}
+						break;
+					}
+					default:
+						'';
+				}
+			}
+		} else {
+			setFieldCommon({ ...fieldCommon, [type]: '' });
+		}
+	};
+	const handleSubmit = async (values: { treasury_address: string }) => {
+		const {
+			key_mint_min_token,
+			key_price,
+			mint_days,
+			rescure_price,
+			launch_price,
+		} = fieldCommon;
+		if (
+			key_mint_min_token !== '0' &&
+			key_price !== '0' &&
+			mint_days !== '0' &&
+			rescure_price !== '0' &&
+			launch_price !== '0'
+		) {
+			const listPromiseFieldChange: Promise<ContractReceipt | undefined>[] = [];
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const objectAfterMerge: any = { ...values, ...fieldCommon };
+			console.log(objectAfterMerge);
+			console.log(initialData);
+			const listKeys = Object.keys(objectAfterMerge);
+			listKeys?.length > 0 &&
+				listKeys.forEach((item) => {
+					switch (item) {
+						case 'treasury_address': {
+							if (objectAfterMerge[item] !== initialData?.[item]) {
+								listPromiseFieldChange.push(
+									...listPromiseFieldChange,
+									updateTreasuryAddressKeyNFTSC(values[item]),
+									updateTreasuryAddressDNFTSC(values[item])
+								);
+							}
+							break;
+						}
+						case 'key_price': {
+							if (parseInt(objectAfterMerge[item]) !== initialData?.[item]) {
+								listPromiseFieldChange.push(
+									updatePriceKeyNFTSC(
+										new BigNumber(objectAfterMerge[item])
+											.times(price.fromBUSDToSC)
+											.toString()
+									)
+								);
+							}
+							break;
+						}
+						case 'rescure_price': {
+							if (parseInt(objectAfterMerge[item]) !== initialData?.[item]) {
+								listPromiseFieldChange.push(
+									updateRescurPriceDNFTSC(
+										new BigNumber(objectAfterMerge[item])
+											.times(price.fromBUSDToSC)
+											.toString()
+									)
+								);
+							}
+							break;
+						}
+						case 'launch_price': {
+							if (
+								parseInt(objectAfterMerge?.[item]) ||
+								'' !== initialData?.[item] ||
+								''
+							) {
+								listPromiseFieldChange.push(
+									...listPromiseFieldChange,
+									updateLaunchPriceDNFTSC(
+										new BigNumber(objectAfterMerge[item])
+											.times(price.fromBUSDToSC)
+											.toString()
+									),
+									updateLaunchPriceKeyNFTSC(
+										new BigNumber(objectAfterMerge[item])
+											.times(price.fromBUSDToSC)
+											.toString()
+									)
+								);
+							}
+							break;
+						}
+						case 'key_mint_min_token': {
+							if (
+								parseInt(objectAfterMerge?.[item]) ||
+								'' !== initialData?.[item] ||
+								''
+							) {
+								listPromiseFieldChange.push(
+									updateMinimumMintKeyDNFTSC(objectAfterMerge[item])
+								);
+							}
+							break;
+						}
+						default:
+							break;
+					}
+				});
+			updateMintDaySystemAdmin({
+				mint_days: parseInt(objectAfterMerge.mint_days),
+			});
+			try {
+				setIsLoadingSystemStatus(true);
+				message.loading('Processing in progress', reloadTime);
+				await Promise.any(listPromiseFieldChange);
+				message.success(MESSAGES.MSC25);
+			} catch (error) {
+				message.error(MESSAGES.MSC26);
+			} finally {
+				setIsLoadingSystemStatus(false);
+				setTimeout(() => {
+					window.location.reload();
+				}, reloadTime);
+			}
+		}
+	};
 	const handleFieldChange = useCallback(
 		(_: FieldData[], allFields: FieldData[]) => {
 			setDisableUpdateBtn(!allFields?.some((item) => item.value));
 		},
 		[]
 	);
-	const handleSubmit = async (values: SubmitProps<number>) => {
-		const listPromiseFieldChange: Promise<ContractReceipt | undefined>[] = [];
-		const listKeys = Object.keys(values);
-		listKeys?.length > 0 &&
-			listKeys.forEach((item) => {
-				switch (item) {
-					case 'treasury_address': {
-						if (values[item] !== initialData?.[item]) {
-							listPromiseFieldChange.push(
-								...listPromiseFieldChange,
-								updateTreasuryAddressKeyNFTSC(values[item]),
-								updateTreasuryAddressDNFTSC(values[item])
-							);
-						}
-						break;
-					}
-					case 'key_price': {
-						if (values[item] !== initialData?.[item]) {
-							listPromiseFieldChange.push(
-								updatePriceKeyNFTSC(
-									new BigNumber(values[item])
-										.times(price.fromBUSDToSC)
-										.toString()
-								)
-							);
-						}
-						break;
-					}
-					case 'rescure_price': {
-						if (values[item] !== initialData?.[item]) {
-							listPromiseFieldChange.push(
-								updateRescurPriceDNFTSC(
-									new BigNumber(values[item])
-										.times(price.fromBUSDToSC)
-										.toString()
-								)
-							);
-						}
-						break;
-					}
-					case 'launch_price': {
-						if (values?.[item] || '' !== initialData?.[item] || '') {
-							listPromiseFieldChange.push(
-								...listPromiseFieldChange,
-								updateLaunchPriceDNFTSC(
-									new BigNumber(values[item])
-										.times(price.fromBUSDToSC)
-										.toString()
-								),
-								updateLaunchPriceKeyNFTSC(
-									new BigNumber(values[item])
-										.times(price.fromBUSDToSC)
-										.toString()
-								)
-							);
-						}
-						break;
-					}
-					case 'key_mint_min_token': {
-						if (values?.[item] || '' !== initialData?.[item] || '') {
-							listPromiseFieldChange.push(
-								updateMinimumMintKeyDNFTSC(values[item])
-							);
-						}
-						break;
-					}
-					default:
-						break;
-				}
-			});
-		updateMintDaySystemAdmin({
-			mint_days: values.mint_days,
-		});
-		try {
-			setIsLoadingSystemStatus(true);
-			message.loading('Processing in progress', reloadTime);
-			await Promise.any(listPromiseFieldChange);
-			message.success(MESSAGES.MSC25);
-		} catch (error) {
-			message.error(MESSAGES.MSC26);
-		} finally {
-			setIsLoadingSystemStatus(false);
-			setTimeout(() => {
-				window.location.reload();
-			}, reloadTime);
-		}
-	};
 	const handleInitialForm = () => {
-		form.setFieldsValue({ ...initialData });
+		form.setFieldsValue({ treasury_address: initialData?.treasury_address });
+	};
+	const handleInitialInput = () => {
+		setFieldCommon({
+			...fieldCommon,
+			key_mint_min_token: String(initialData?.key_mint_min_token || 0) || '0',
+			mint_days: String(initialData?.mint_days || 0) || '0',
+			launch_price: String(initialData?.launch_price || 0) || '0',
+			rescure_price: String(initialData?.rescure_price || 0) || '0',
+			key_price: String(initialData?.key_price || 0) || '0',
+		});
 	};
 	useEffect(() => {
 		handleInitialForm();
+		handleInitialInput();
 	}, [initialData]);
-
 	return {
-		disableUpdateBtn,
 		handleFieldChange,
 		isLoadingSystemStatus,
 		handleSubmit,
+		disableUpdateBtn,
 		price,
+		handleRegexField,
+		initialData,
+		fieldCommon,
 		form,
 	};
 };
