38916c4c1e4964eb928b0a14c18247cc5f1f1113
Hiep Nguyen Van <hiep.nguyen2@sotatek.com>
2022-11-15 15:00:37 +0700

    fix: image placeholder

--- src/modules/wallet/connect/components/Logo/Logo.tsx
+++ src/modules/wallet/connect/components/Logo/Logo.tsx
@@ -7,7 +7,7 @@ const { Title } = Typography;
 export default function Logo() {
 	return (
 		<Title level={2} className='app-logo'>
-			<img src={AppLogo} alt='' />
+			<img src={AppLogo} alt='' width={82} height={79} />
 		</Title>
 	);
 }
