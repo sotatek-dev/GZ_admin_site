1e2a9d80857d890fcddaf93cd5622a8d8bc333f1
thien.nguyen6 <thiennguyen6@thien.nguyen6>
2022-11-21 11:05:42 +0700

    fix: up

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday';
+import localeData from 'dayjs/plugin/localeData';
+
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
