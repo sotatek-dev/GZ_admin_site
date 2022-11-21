036ce2972f997a2d8566013e455a41b21c4fc075
thien.nguyen6 <thiennguyen6@thien.nguyen6>
2022-11-21 10:07:41 +0700

    fix: extend dayjs plugin

--- src/modules/common/components/DatePicker/DatePicker.tsx
+++ src/modules/common/components/DatePicker/DatePicker.tsx
@@ -1,6 +1,11 @@
-import { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs';
 import generatePicker from 'antd/es/date-picker/generatePicker';
+import weekday from 'dayjs/plugin/weekday';
+import localeData from 'dayjs/plugin/localeData';
+
+dayjs.extend(weekday);
+dayjs.extend(localeData);
 
 const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
 
